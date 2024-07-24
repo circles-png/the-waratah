e6646741bcb350d8bccea83e5b9a6991cabbc2ee
Angus Mason <angus.mason@me.com>
2024-07-25 09:05:29 +1000

    discliaimer agian

--- src/components.rs
+++ src/components.rs
@@ -52,7 +52,7 @@ pub fn Disclaimer() -> impl IntoView {
     view! {
         <div class="flex flex-col">
             <Heading>Disclaimer</Heading>
-            "The Yesterday disclaims anything and everything, solely to avoid all responsibility for anything and everything."
+            "The Yesterday disclaims anything and everything, in order to avoid all responsibility for it all."
         </div>
     }
 }
